be8308b0bfbe3b2edf881d3410ec79770f813b17
gracelandolfi <glandolfi@icloud.com>
2024-09-06 13:50:18 -0400

    Ch. 1.12 A Beginner's Guide to Swift Types + Flipping Through SwiftUI Images (You Are Awesome app)

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
     @State private var messageString = ""
     @State private var imageName = ""
+    @State private var imageNumber = 0
+    
     var body: some View {
         VStack {
             
@@ -44,15 +46,16 @@ struct ContentView: View {
                 
                 messageString = (messageString == message1 ? message2 : message1)
                 
-                imageName = (imageName == "image0" ? "image1" : "image0")
+                //                imageName = (imageName == "image0" ? "image1" : "image0")
+                
+                //TODO: - update the imageName variable -
+                imageName = "image" + imageNumber
+                imageNumber = imageNumber + 1
+                if imageNumber > 9 {
+                    imageNumber = 0
+                }
+                print(imageNumber)
             }
-            //
-            //                    if messageString == message1 {
-            //                        messageString = message2
-            //                    } else {
-            //                        messageString = message1
-            //                    }
-            //                }
             .buttonStyle(.borderedProminent)
             
             
