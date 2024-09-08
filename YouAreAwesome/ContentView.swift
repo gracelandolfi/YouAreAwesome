d23dd56bb2cd43d764d6d4de40dd4499b6595989
gracelandolfi <glandolfi@icloud.com>
2024-09-08 11:18:06 -0400

    Ch. 1.15 while and repeat while loops in Swift

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,24 +10,11 @@ import SwiftUI
 struct ContentView: View {
     @State private var messageString = ""
     @State private var imageName = ""
-    @State private var imageNumber = 0
-    @State private var messageNumber = 0
+    @State private var lastMessageNumber = -1
+    @State private var lastImageNumber = -1
     
     var body: some View {
         VStack {
-            
-            
-            Image(imageName)
-                .resizable()
-                .scaledToFit()
-                .cornerRadius(30)
-                .shadow(radius: 30)
-                .padding()
-            
-            Spacer()
-            
-            
-            
             Text(messageString)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
@@ -36,6 +23,19 @@ struct ContentView: View {
                 .padding()
                 .frame(height: 150)
                 .frame(maxWidth: .infinity)
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(30)
+                .shadow(radius: 30)
+                .padding()
+            
+            
+            
+            
+            
+            
             
             
             Spacer()
@@ -46,25 +46,39 @@ struct ContentView: View {
                                 "You Are Great!",
                                 "Fabulous! That's You!",
                                 "You Rule!"]
+                
+                var messageNumber = Int.random(in: 0...messages.count-1)
+                while messageNumber == lastMessageNumber {
+                    messageNumber = Int.random(in: 0...messages.count-1)
+                }
                 messageString = messages[messageNumber]
-                messageNumber = messageNumber + 1
+                lastMessageNumber = messageNumber
                 
-                if messageNumber == messages.count {
-                    messageNumber = 0
+                var imageNumber = Int.random(in: 0...9)
+                while imageNumber == lastImageNumber {
+                    imageNumber = Int.random(in: 0...9)
                 }
+                imageName = "image \(imageNumber)"
+                lastImageNumber = imageNumber
                 
-                
-                //      messageString = (messageString == message1 ? message2 : message1)
-                
-                //                imageName = (imageName == "image0" ? "image1" : "image0")
-                
-                //TODO: - update the imageName variable -
-                imageName = "image\(imageNumber)"
-                imageNumber = imageNumber + 1
-                if imageNumber > 9 {
-                    imageNumber = 0
+//                messageString = messages[messageNumber]
+//                messageNumber = messageNumber + 1
+//                
+//                if messageNumber == messages.count {
+//                    messageNumber = 0
+//                }
+//                
+//                
+//                //      messageString = (messageString == message1 ? message2 : message1)
+//                
+//                //                imageName = (imageName == "image0" ? "image1" : "image0")
+//                
+//                //TODO: - update the imageName variable -
+//                imageName = "image\(imageNumber)"
+//                imageNumber = imageNumber + 1
+//                if imageNumber > 9 {
+//                    imageNumber = 0
                 }
-                print(imageNumber)
             }
             .buttonStyle(.borderedProminent)
             
@@ -74,7 +88,7 @@ struct ContentView: View {
             
         }
     }
-}
+
 
 
 #Preview {
